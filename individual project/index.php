48ef1194c244a325d1a52a43c93da44a35b7b574
DineshaTennakoon <129361319+DineshaTennakoon@users.noreply.github.com>
2023-04-20 11:57:47 +0530

    Add files via upload

---
+++ individual project/index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="homepage.png">
+    <title>Home</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet"> </head>
+
+<body class="home">
+    
+        <!--header starts-->
+        <header id="header" class="header-scroll top-header headrom">
+            <!-- .navbar -->
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/food-picky-logo.png" alt=""> </a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="">Restaurants <span class="sr-only"></span></a> </li>
+                            
+                        </ul>
+						 
+                    </div>
+                </div>
+            </nav>
+            <!-- /.navbar -->
+        </header>
+        <!-- banner part starts -->
+        <section class="hero bg-image" data-image-src="images/img/main.jpeg">
+            <div class="hero-inner">
+                <div class="container text-center hero-text font-white">
+                    <h1>Order your favourite meal</h1>
+                    <h5 class="font-white space-xs">Find restaurants and dishes</h5>
+                    <div class="banner-form">
+                        <form class="form-inline">
+                            <div class="form-group">
+                                <label class="sr-only" for="exampleInputAmount">I would like to eat....</label>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-lg" id="exampleInputAmount" placeholder="I would like to eat...."> </div>
+                            </div>
+                            <button onclick="location.href=" type="button" class="btn theme-btn btn-lg">Search</button>
+                        </form>
+                    </div>
+                   
+                    <!-- end:Steps -->
+                </div>
+            </div>
+            <!--end:Hero inner -->
+        </section>
+        <!-- banner part ends -->
+      
+	  
+	
+        <!-- Popular block starts -->
+        <section class="popular">
+            <div class="container">
+                <div class="title text-xs-center m-b-30">
+                    <h2>Trending Dishes of this Week</h2>
+                    <p class="lead">Your favourite dishes are here...</p>
+                </div>
+                <div class="row">
+				
+                </div>
+            </div>
+        </section>
+        <!-- Popular block ends -->
+        <!-- How it works block starts -->
+        <section class="how-it-works">
+            <div class="container">
+                <div class="text-xs-center">
+                    <h2>    Get ordered in simple steps</h2>
+                    <!-- 3 block sections starts -->
+                    <div class="row how-it-works-solution">
+                        <div class="col-xs-12 col-sm-12 col-md-4 how-it-works-steps white-txt col1">
+                            <div class="how-it-works-wrap">
+                                <div class="step step-1">
+                                   
+                                    <h3>1.Choose a restaurant</h3>
+                                    <p>More than 100+ popular delivery restaurants.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 how-it-works-steps white-txt col2">
+                            <div class="step step-2">
+                               
+                                
+                                <h3>2.Choose your favorite meal</h3>
+                                <p>More than 3000+ popular meal varieties.</p>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 how-it-works-steps white-txt col3">
+                            <div class="step step-3">
+                               
+                                <h3> 3.Delivery & Payment</h3>
+                                <p>Get delivered within 30 minutes.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- 3 block sections ends -->
+                <div class="row">
+                    <div class="col-sm-12 text-center">
+                        <p class="pay-info">Pay by Cash on delivery or Card Payment</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- How it works block ends -->
+        <
+        <section class="web-section">
+            <div class="web-wrap">
+                <div class="container">
+                    <div class="row text-img-block text-xs-left">
+                        <div class="container">
+                            <div class="col-xs-12 col-sm-5 right-image text-center">
+                                <figure> <img src="images\img\web.jpg" alt="Right Image" class="img-fluid"> </figure>
+                            </div>
+                            <div class="col-xs-12 col-sm-7 left-text">
+                                <h3>The Best Food Delivery System</h3>
+                                <p>Got Hungry? Get the food you want, from the restaurants you love, delivered at blinking speed.
+
+								Eat what you like, where you like, when you like. Find the local flavours you crave, all at the tap of a button.</p>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- start: FOOTER -->
+        <footer class="footer">
+            <div class="container">
+                <!-- top footer statrs -->
+                <div class="row top-footer">
+                    <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
+                       <span>Choose & &amp; Enjoy your meals! </span> </div>
+                    <div class="col-xs-12 col-sm-2 about color-gray">
+                        <h5>About Us</h5>
+                        <ul>
+                            <li><a href="#">Our Mission</a></li>
+                            <li><a href="#">Testimonials</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
+                        <h5>How it Works?</h5>
+                        <ul>
+                            <li><a href="#">Enter your location</a></li>
+                            <li><a href="#">Choose the restaurant</a></li>
+                            <li><a href="#">Choose your dishes</a></li>
+                            <li><a href="#">Get delivered</a></li>
+							<li><a href="#">Pay on delivery</a></li>
+							<li><a href="#">Enjoy your meals</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-2 pages color-gray">
+                        <h5>Legal</h5>
+                        <ul>
+                            <li><a href="#">Terms & Conditions</a> </li>
+                            <li><a href="#">Refund & Cancellation</a> </li>
+                            <li><a href="#">Privacy Policy</a> </li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 popular-locations color-gray">
+                        <h5>Locations We Deliver To</h5>
+                        <ul>
+                            <li><a href="#">Kurunegala</a> </li>
+                            <li><a href="#">Giriula</a> </li>
+                           
+                        </ul>
+                    </div>
+                </div>
+                <!-- top footer ends -->
+                <!-- bottom footer statrs -->
+                <div class="bottom-footer">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                            <h5>All Major Credit Cards Accepted</h5>
+                            <ul>
+                                <li>
+                                    <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                                </li>
+                               
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 address color-gray">
+                            <h5>Address:</h5>
+                            <p>Kurunegala,Sri Lanka</p>
+                            <h5>Call us at: <a href="tel:+94765437822">+971234658</a></h5></div>
+                        <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                            <h5>About us</h5>
+                            <p>This platform connects customers, restaurant partners and delivery partners, serving their multiple needs. Customers use our platform to search and discover restaurants, read and write customer generated reviews and view and upload photos, order food delivery, book a table and make payments while dining-out at restaurants.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- bottom footer ends -->
+            </div>
+        </footer>
+        <!-- end:Footer -->
+    
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
